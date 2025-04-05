b30bfaa5c1697c25c49e8a89a8c848f3bad2b64f
DingusBingo <christhehurley@gmail.com>
2025-04-05 01:08:26 -0400

    point and line classes, fixed window

--- venv/PROGRAM/main.py
+++ venv/PROGRAM/main.py
@@ -1,4 +1,16 @@
-from window import Window
+from window import Window, Point, Line
 
 win = Window(800, 600)
+
+pA1 = Point(400, 0)
+pA2 = Point(400, 600)
+pB1 = Point(0, 300)
+pB2 = Point(800, 300)
+
+l1 = Line(pA1, pA2)
+l2 = Line(pB1, pB2)
+
+win.draw_line(l1, 'black')
+win.draw_line(l2, 'black')
+
 win.wait_for_close()
--- venv/PROGRAM/window.py
+++ venv/PROGRAM/window.py
@@ -6,7 +6,7 @@ class Window:
         self.height = height
         self.__root = Tk()
         self.__root.title = "Window"
-        self.canvas = Canvas()
+        self.canvas = Canvas(self.__root, width=self.width, height=self.height)
         self.canvas.pack()
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -23,3 +23,21 @@ class Window:
     
     def close(self):
         self.running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)

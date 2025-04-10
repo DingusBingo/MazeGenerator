f19de76430dbe46a6ce71d4da51f3a9524bcb5bc
DingusBingo <christhehurley@gmail.com>
2025-04-09 23:26:32 -0400

    fixed exit break

--- venv/PROGRAM/cell.py
+++ venv/PROGRAM/cell.py
@@ -20,7 +20,7 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        
+
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
@@ -39,14 +39,14 @@ class Cell:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
         else:
-            line = Line(Point(x1, y1), Point(x1, y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line, "white")
 
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
         else:
-            line = Line(Point(x1, y1), Point(x1, y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
